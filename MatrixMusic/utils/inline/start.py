5b5f2edf343de51b506e1319db055d8659d35ecd
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-07-12 07:42:44 +0300

    B2
    B2

---
+++ MatrixMusic/utils/inline/start.py
@@ -0,0 +1,42 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from MatrixMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startchannel=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/cecrr",
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL)
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startchannel=true"),
+        ],
+    ]
+    return buttons
